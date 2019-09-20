a13eb494b648af8a68a97aaa91f52c2b95fa09ef
Mitchell <soggybag@gmail.com>
2019-09-20 09:46:08 -0700

    Updates

---
+++ precache-manifest.5ce9c4e82073d65689803bfada1dcdc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a2cc3012ea4b679b21576fa78a5c6d",
+    "url": "/soggybag/simple-journal/index.html"
+  },
+  {
+    "revision": "86f2f2b772a33f0b734e",
+    "url": "/soggybag/simple-journal/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1023d2cc04a7682decb6",
+    "url": "/soggybag/simple-journal/static/js/2.9eaf9848.chunk.js"
+  },
+  {
+    "revision": "86f2f2b772a33f0b734e",
+    "url": "/soggybag/simple-journal/static/js/main.92e81887.chunk.js"
+  },
+  {
+    "revision": "62f76b6972ff129c2a84",
+    "url": "/soggybag/simple-journal/static/js/runtime~main.ea776e0f.js"
+  }
+]);
